8e1d863e6d96e687f3802c93e1b0496f5385306a
Nicolas Hadacek <hadacek@kde.org>
2001-12-20 20:18:48 +0000

    fix url !!
    svn path=/trunk/kdegames/kmines/; revision=128133

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define VERSION	     "2.1.4"
-#define LONG_VERSION "2.1.4 (12 December 2001)"
+#define LONG_VERSION "2.1.4 (16 December 2001)"
 #define COPYLEFT     "(c) 1996-2001, Nicolas Hadacek"
 #define EMAIL        "hadacek@kde.org"
 
-#define HOMEPAGE "http://kmines.sourceforge.net"
+#define HOMEPAGE "http://kmines.sourceforge.net/"
