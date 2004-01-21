5b3e7214a756b7f08ea8d7fa712fe106d7a53044
Dirk Mueller <mueller@kde.org>
2004-01-21 06:58:41 +0000

    fix KURL handling errors
    svn path=/trunk/kdegames/kmines/; revision=281475

--- highscores.cpp
+++ highscores.cpp
@@ -34,7 +34,7 @@ ExtManager::ExtManager()
     : Manager(Level::NB_TYPES)
 {
     setScoreType(MinuteTime);
-    setWWHighscores(HOMEPAGE, VERSION);
+    setWWHighscores(KURL( HOMEPAGE ), VERSION);
     showStatistics(true);
     const uint RANGE[16] = {    1, 3120, 3180, 3240, 3300, 3360, 3420, 3480,
                              3510, 3540, 3550, 3560, 3570, 3580, 3590, 3600  };
