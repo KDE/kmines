c2d1ba317a3025aad25e006b2bb2eb832e8ccb3d
Nicolas Hadacek <hadacek@kde.org>
2004-02-29 18:40:05 +0000

    update changelog and version
    svn path=/branches/KDE_3_2_BRANCH/kdegames/kmines/; revision=292601

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-#define VERSION	     "2.1.8"
-#define LONG_VERSION "2.1.8 (3 December 2002)"
-#define COPYLEFT     "(c) 1996-2002, Nicolas Hadacek\n(c) 2001, Mikhail Kourinny"
+#define VERSION	     "2.1.8b"
+#define LONG_VERSION "2.1.8b (26 February 2004)"
+#define COPYLEFT     "(c) 1996-2004, Nicolas Hadacek\n(c) 2001, Mikhail Kourinny"
 #define EMAIL        "hadacek@kde.org"
 
 #define HOMEPAGE "http://kmines.sourceforge.net/"
