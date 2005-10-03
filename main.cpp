48a5b20c9c430c89b2a42821d9c2b7814a7b507d
Stephan Kulow <coolo@kde.org>
2005-10-03 17:46:54 +0000

    first round of porting
    svn path=/trunk/KDE/kdegames/kmines/; revision=466923

--- main.cpp
+++ main.cpp
@@ -251,7 +251,7 @@ int main(int argc, char **argv)
     KCmdLineArgs::init(argc, argv, &aboutData);
 
     KApplication a;
-    KGlobal::locale()->insertCatalogue("libkdegames");
+    KGlobal::locale()->insertCatalog("libkdegames");
     KExtHighscore::ExtManager manager;
 
     if ( a.isRestored() ) RESTORE(MainWidget)
