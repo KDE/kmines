0559a5f0dab54539e9e5ce0a545459203ce88b45
Mauricio Piacentini <piacentini@kde.org>
2007-02-14 16:34:46 +0000

    Merging the older Frame and Field classes into  a single object, the new central place where game board drawing takes place.
    svn path=/trunk/KDE/kdegames/kmines/; revision=633630

--- field.cpp
+++ field.cpp
@@ -26,14 +26,15 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QPaintEvent>
+#include <QStyleOptionFocusRect>
 
 #include <klocale.h>
 #include <knotification.h>
+#include <kstandarddirs.h>
 
 #include "settings.h"
 #include "solver/solver.h"
 #include "dialogs.h"
-
 
 using namespace KGrid2D;
 
@@ -47,9 +48,15 @@ const Field::ActionData Field::ACTION_DATA[Nb_Actions] = {
 };
 
 Field::Field(QWidget *parent)
-    : FieldFrame(parent), _state(Init), _solvingState(Regular), _level(Level::Easy)
+    : QWidget(parent), _state(Init), _solvingState(Regular), _level(Level::Easy)
 {
     borderSize = Settings::caseSize();
+
+    QString themePath = KStandardDirs::locate("appdata", QString("themes/kmines_classic.svgz"));
+    if (themePath.isNull()) {
+        qDebug () << "theme svg not found!!!";
+    };
+    svg.load(themePath);
 }
 
 void Field::readSettings()
@@ -495,4 +502,111 @@ void Field::drawCase(QPainter &painter, const Coord &c, bool pressed)
 
     bool hasFocus = ( Settings::keyboardGame() && (c==_cursor) );
     drawBox(painter, toPoint(c), pressed, type, text, nbMines, i, hasFocus);
+}
+
+void Field::adjustSize()
+{
+    setFixedSize(sizeHint());
+    _button.resize(Settings::caseSize(), Settings::caseSize());
+
+    QPixmap mask;
+    for (uint i=0; i<Nb_Pixmap_Types; i++) {
+        drawPixmap(_pixmaps[i], (PixmapType)i, true);
+    }
+    for (uint i=0; i<Nb_Advised; i++) {
+        drawAdvised(_advised[i], i, true);;
+    }
+
+    QFont f = font();
+    f.setPointSize(qMax(1, Settings::caseSize()-6));
+    f.setBold(true);
+    setFont(f);
+}
+
+void Field::initPixmap(QPixmap &pix, bool mask) 
+{
+    pix = QPixmap( Settings::caseSize(), Settings::caseSize() );
+    if (mask) pix.fill(Qt::color0);
+}
+
+void Field::drawPixmap(QPixmap &pix, PixmapType type, bool mask) 
+{
+    initPixmap(pix, mask);
+
+    QImage qiRend(QSize(pix.width(), pix.height()),QImage::Format_ARGB32_Premultiplied);
+    qiRend.fill(0);
+    QPainter p(&qiRend);
+
+    if ( type==FlagPixmap ) {
+        svg.render(&p, "flag");
+        pix = QPixmap::fromImage(qiRend);
+        return;
+    }
+
+    //If exploding...;
+    if ( type==ExplodedPixmap ) {
+        svg.render(&p, "explosion");;
+    }
+
+    //Now render mine graphic
+    svg.render(&p, "mine");
+
+    //Finally render the error marker
+    if ( type==ErrorPixmap ) {
+      svg.render(&p, "error");
+    }
+    pix = QPixmap::fromImage(qiRend);
+}
+
+void Field::drawAdvised(QPixmap &pix, uint i, bool mask) 
+{
+    initPixmap(pix, mask);
+    QImage qiRend(QSize(pix.width(), pix.height()),QImage::Format_ARGB32_Premultiplied);
+    qiRend.fill(0);
+    QPainter p(&qiRend);
+    /*QPainter p(&pix);
+    p.setWindow(0, 0, 16, 16);
+    p.setPen( QPen(mask ? Qt::color1 : Settings::mineColor(i), 2) );
+    p.drawRect(3, 3, 11, 11);*/
+    svg.render(&p, "hint");
+    pix = QPixmap::fromImage(qiRend);
+}
+
+void Field::drawBox(QPainter &painter, const QPoint &p,
+                      bool pressed, PixmapType type, const QString &text,
+                      uint nbMines, int advised,
+                      bool hasFocus) 
+{
+    //Use SVG theme instead
+    /*qDrawShadePanel(&painter, p.x(), p.y(), _button.width(), _button.height(),
+                    palette(),  pressed, 2,
+                    &palette().brush(QPalette::Background));
+
+    if (hasFocus) {
+        painter.translate(p.x(), p.y());
+        QStyleOptionFocusRect option;
+        option.init(this);
+        option.fontMetrics = painter.fontMetrics();
+        option.state = QStyle::State_Enabled;
+        QRect fbr = style()->subElementRect(QStyle::SE_PushButtonFocusRect, &option, &_button);
+        option.rect = fbr;
+        style()->drawPrimitive(QStyle::PE_FrameFocusRect, &option, &painter);
+        painter.resetMatrix();
+    }*/
+
+    QRect r(p, _button.size());
+    if (pressed) {
+        svg.render(&painter, "cell_down", r);
+    } else {
+        svg.render(&painter, "cell_up", r);
+    }
+    const QPixmap *pixmap = (type==NoPixmap ? 0 : &_pixmaps[type]);
+    QColor color = (nbMines==0 ? Qt::black : Settings::mineColor(nbMines-1));
+    QPalette pal;
+    pal.setColor( QPalette::WindowText, color );
+    style()->drawItemText(&painter, r, Qt::AlignCenter, pal, true, text, QPalette::WindowText);
+    if (pixmap)
+      style()->drawItemPixmap(&painter, r, Qt::AlignCenter, *pixmap);
+    if ( advised!=-1 )
+        style()->drawItemPixmap(&painter, r, Qt::AlignCenter, _advised[advised]);
 }
