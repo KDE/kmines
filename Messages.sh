9ab6e09170845f199a7dc99233b3d02d8a6df0d2
Dmitry Suzdalev <dimsuz@gmail.com>
2007-07-17 14:13:45 +0000

    Deleted old code. Long live new KMines :)
    svn path=/trunk/KDE/kdegames/kmines/; revision=689063

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-$XGETTEXT rc.cpp *.cpp solver/*.cpp -o $podir/kmines.pot
+$XGETTEXT rc.cpp *.cpp -o $podir/kmines.pot
--- settings_addons.h
+++
@@ -1,5 +0,0 @@
-public:
-  static Level customLevel()
-  {
-    return Level( self()->mCustomWidth, self()->mCustomHeight, self()->mCustomMines );
-  }
