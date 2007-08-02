998f604d6314d6b0f017ed9885abf85c368e6bc1
Dmitry Suzdalev <dimsuz@gmail.com>
2007-08-02 10:31:42 +0000

    Change version or we surely will forget it ;) Also added this version on kmines product on b.k.o
    svn path=/trunk/KDE/kdegames/kmines/; revision=695538

--- version.h
+++ version.h
@@ -17,8 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#define VERSION	     "2.99"
-#define LONG_VERSION "2.99 (17 Jul 2007)"
+#define VERSION	     "3.0"
+#define LONG_VERSION "3.0 (02 Aug 2007)"
 #define COPYLEFT     "(c) 1996-2005, Nicolas Hadacek\n(c) 2001, Mikhail Kourinny\n(c) 2006-2007, Mauricio Piacentini\n(c) 2007, Dmitry Suzdalev"
 #define EMAIL        "hadacek@kde.org"
 #define HOMEPAGE     "http://kmines.sourceforge.net/"
