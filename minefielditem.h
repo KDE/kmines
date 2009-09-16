3e6cc7651804cfd3db011313d5d1a50412871b14
Laurent Montel <montel@kde.org>
2009-09-16 17:35:20 +0000

    It breaks on qt < 4.6
    svn path=/trunk/KDE/kdegames/kmines/; revision=1024499

--- minefielditem.h
+++ minefielditem.h
@@ -38,7 +38,9 @@ typedef QPair<int,int> FieldPos;
 class MineFieldItem : public QObject, public QGraphicsItem
 {
     Q_OBJECT
+#if QT_VERSION >= 0x040600	    
     Q_INTERFACES(QGraphicsItem)
+#endif    
 public:
     /**
      * Constructor.
