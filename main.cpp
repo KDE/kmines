fc6f516a63bda8cd193e7b3e64d0e14062c6a319
Montel Laurent <montel@kde.org>
2017-10-23 14:00:39 +0200

    it's already done in kaboutdata

--- main.cpp
+++ main.cpp
@@ -68,8 +68,6 @@ int main(int argc, char **argv)
     KAboutData::setApplicationData(aboutData);
     KCrash::initialize();
     QCommandLineParser parser;
-    parser.addVersionOption();
-    parser.addHelpOption();
     aboutData.setupCommandLine(&parser);
     parser.process(app);
     aboutData.processCommandLine(&parser);
